52035d1ec7a648c33b4cf8bac26118905db71c78
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2019-05-11 01:19:31 +0200

    update

--- worldofbooks/settings.py
+++ worldofbooks/settings.py
@@ -97,13 +97,8 @@ WSGI_APPLICATION = 'worldofbooks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
-        'NAME'     : 'WorldOfBooks',
-        'USER'     : 'postgres',
-        'PASSWORD' : 'mahmoudaboelnaga@1751998',
-        'HOST'     : '127.0.0.1',
-        'PORT'     : '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
